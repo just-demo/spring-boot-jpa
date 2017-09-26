017d53097e344d57a6081f570a6a094ae14ed48e
Anatolii <anatolii.kryvda@gmail.com>
2017-09-27 00:32:39 +0300

    Jersey rest API

---
+++ src/main/java/self/edu/servlet/TestHttpServlet.java
@@ -0,0 +1,24 @@
+package self.edu.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Anatolii
+ */
+@WebServlet("/servlet")
+public class TestHttpServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.print("<html><body>");
+        out.print("<h3>Hello Servlet!!2!</h3>");
+        out.print("</body></html>");
+    }
+}
