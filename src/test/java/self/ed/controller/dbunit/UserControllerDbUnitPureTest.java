4c0e02ad347e753b06ff91c4077529dad5b9ab0b
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2018-07-26 14:28:25 +0300

    Adding more pure db unit test

--- src/test/java/self/ed/controller/dbunit/UserControllerDbUnitPureTest.java
+++ src/test/java/self/ed/controller/dbunit/UserControllerDbUnitPureTest.java
@@ -1,9 +1,10 @@
 package self.ed.controller.dbunit;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.dbunit.Assertion;
-import org.dbunit.DataSourceDatabaseTester;
-import org.dbunit.IDatabaseTester;
-import org.dbunit.dataset.ColumnFilterTable;
+import org.dbunit.database.DatabaseDataSourceConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.Column;
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
@@ -18,17 +19,21 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import self.ed.entity.User;
-import self.ed.testing.support.EntityHelper;
 
 import javax.sql.DataSource;
-import java.net.URL;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.dbunit.dataset.filter.DefaultColumnFilter.includedColumnsTable;
+import static org.dbunit.operation.DatabaseOperation.CLEAN_INSERT;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 /**
  * @author Anatolii
@@ -42,138 +47,88 @@ public class UserControllerDbUnitPureTest {
     private static final String PATH_USER = "/users/{id}";
 
     @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
     private DataSource dataSource;
 
-    private IDatabaseTester databaseTester;
+    private IDatabaseConnection connection;
 
     @Before
-    public void setUp() {
-        databaseTester = new DataSourceDatabaseTester(dataSource);
+    public void setUp() throws Exception {
+        this.connection = new DatabaseDataSourceConnection(dataSource);
     }
 
     @After
     public void tearDown() throws Exception {
-        databaseTester.onTearDown();
+        connection.close();
     }
-//
-//    protected void setUp2() throws Exception
-//    {
-//
-//        // Execute the tested code that modify the database here
-//        ...
-//
-//
-//        // Fetch database data after executing your code
-//        IDataSet databaseDataSet = getConnection().createDataSet();
-//        ITable actualTable = databaseDataSet.getTable("TABLE_NAME");
-//
-//        // Load expected data from an XML dataset
-//        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("expectedDataSet.xml"));
-//        ITable expectedTable = expectedDataSet.getTable("TABLE_NAME");
-//
-//        // Assert actual database table match expected table
-//        Assertion.assertEquals(expectedTable, actualTable);
-//
-//
-//        ITable actualJoinData = getConnection().createQueryTable("RESULT_NAME",
-//                "SELECT * FROM TABLE1, TABLE2 WHERE ...");
-//
-//
-//        ITable filteredTable = DefaultColumnFilter.includedColumnsTable(actual,
-//                expected.getTableMetaData().getColumns());
-//        Assertion.assertEquals(expected, filteredTable);
-//
-//        super.setUp();
-//
-//        // initialize your database connection here
-//        IDatabaseConnection connection = null;
-//        // ...
-//
-//        // initialize your dataset here
-//        IDataSet dataSet = null;
-//        // ...
-//
-//        try1
-//        {
-//            DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
-//        }
-//        finally
-//        {
-//            connection.close();
-//        }
-//    }
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+    @Test
+    public void testFindAll() throws Exception {
+        IDataSet dataSet = readFile("user.multiple.xml");
+        CLEAN_INSERT.execute(connection, dataSet);
 
-    @Autowired
-    private EntityHelper entityHelper;
-//
-//    @Test
-//    public void testFindAll() {
-//        databaseTester = new DataSourceDatabaseTester(dataSource);
-//        IDataSet dataSet = new FlatXmlDataSetBuilder().build(new File("user.multiple.xml"));
-//        databaseTester.setDataSet( dataSet );
-//        databaseTester.onSetup();
-//
-//        ResponseEntity<User[]> entity = restTemplate.getForEntity(PATH_USERS, User[].class);
-//
-//        assertThat(entity.getStatusCode()).isEqualTo(OK);
-//
-//        IDataSet databaseDataSet = databaseTester.getConnection().createDataSet();
-//        ITable actualUsers = databaseDataSet.getTable("user");
-//
-//        ITable expectedUsers = new FlatXmlDataSetBuilder().build(new File("user.multiple.xml")).getTable("user");
-//
-//
-//        entity.getBody()
-//
-//        List<User> expectedUsers = entityHelper.findAll(User.class);
-//        assertThat(entity.getBody()).containsOnlyElementsOf(expectedUsers);
-//    }
-//
-//    @Test
-//    @DatabaseSetup(value = "user.multiple.xml")
-//    public void testFind() {
-//        ResponseEntity<User> entity = restTemplate.getForEntity(PATH_USER, User.class, 1L);
-//
-//        assertThat(entity.getStatusCode()).isEqualTo(OK);
-//        User expectedUser = entityHelper.find(User.class, 1L);
-//        assertThat(entity.getBody()).isEqualTo(expectedUser);
-//    }
+        ResponseEntity<User[]> entity = restTemplate.getForEntity(PATH_USERS, User[].class);
+
+        assertThat(entity.getStatusCode()).isEqualTo(OK);
+
+        List<User> expectedUsers = convertTableToList(dataSet.getTable("user"), User.class);
+        assertThat(entity.getBody()).containsOnlyElementsOf(expectedUsers);
+    }
+
+    @Test
+    public void testFind() throws Exception {
+        IDataSet dataSet = readFile("user.multiple.xml");
+        CLEAN_INSERT.execute(connection, dataSet);
+
+        ResponseEntity<User> entity = restTemplate.getForEntity(PATH_USER, User.class, 1L);
+
+        assertThat(entity.getStatusCode()).isEqualTo(OK);
+
+        User expectedUser = convertTableToList(dataSet.getTable("user"), User.class).stream()
+                .filter(user -> user.getId().equals(1L))
+                .findAny()
+                .orElseThrow(RuntimeException::new);
+        assertThat(entity.getBody()).isEqualTo(expectedUser);
+    }
 
     @Test
     public void testCreate() throws Exception {
-        databaseTester = new DataSourceDatabaseTester(dataSource);
         IDataSet dataSet = readFile("user.empty.xml");
-        databaseTester.setDataSet(dataSet);
-        databaseTester.onSetup();
+        CLEAN_INSERT.execute(connection, dataSet);
         User user = new User();
         user.setName("user1");
 
         ResponseEntity<User> entity = restTemplate.postForEntity(PATH_USERS, user, User.class);
 
         assertThat(entity.getStatusCode()).isEqualTo(CREATED);
-        ITable actualUsers = databaseTester.getConnection().createDataSet().getTable("user");
-        actualUsers = filterColumns(actualUsers, "name");
         ITable expectedUsers = readFile("user.single.xml").getTable("user");
-        //        ITable actualJoinData = getConnection().createQueryTable("RESULT_NAME",
-        //                "SELECT * FROM TABLE1, TABLE2 WHERE ...");
+        ITable actualUsers = connection.createDataSet().getTable("user");
+        actualUsers = includedColumnsTable(actualUsers, expectedUsers.getTableMetaData().getColumns());
         Assertion.assertEquals(expectedUsers, actualUsers);
     }
 
-    private ITable filterColumns(ITable table, String... columns) throws DataSetException {
-        Set<String> columnSet = stream(columns)
-                .map(String::toLowerCase)
-                .collect(toSet());
-        return new ColumnFilterTable(table, (tableName, column) -> columnSet.contains(column.getColumnName().toLowerCase()));
-    }
-
     private IDataSet readFile(String fileName) throws DataSetException {
-        return new FlatXmlDataSetBuilder().build(getResource(fileName));
+        return new FlatXmlDataSetBuilder().build(this.getClass().getResource(fileName));
     }
 
-    private URL getResource(String fileName) {
-        return this.getClass().getResource(fileName);
+    private <T> List<T> convertTableToList(ITable table, Class<T> clazz) throws Exception {
+        List<String> columns = stream(table.getTableMetaData().getColumns())
+                .map(Column::getColumnName)
+                .collect(toList());
+
+        List<T> entities = new ArrayList<>();
+        int rowCount = table.getRowCount();
+        for (int row = 0; row < rowCount; row++) {
+            Map<String, Object> fields = new HashMap<>();
+            for (String column : columns) {
+                fields.put(column, table.getValue(row, column));
+            }
+            T entity = clazz.newInstance();
+            BeanUtils.populate(entity, fields);
+            entities.add(entity);
+        }
+        return entities;
     }
 }
