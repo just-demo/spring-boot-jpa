437b44c51e8e43281f56400126fca7008ac7cae8
Anatolii <anatolii.kryvda@gmail.com>
2018-08-01 00:22:28 +0300

    add more complex repository methods

--- src/main/java/self/ed/repository/UserRepository.java
+++ src/main/java/self/ed/repository/UserRepository.java
@@ -1,10 +1,23 @@
 package self.ed.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 import self.ed.entity.User;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Anatolii
  */
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    Page<User> findAllByName(String name, Pageable pageable);
+    List<User> findAllByNameIn(Collection<String> names);
+    long countByName(String name);
+    @Transactional
+    long deleteByName(String name);
+    @Transactional
+    List<User> removeByName(String name);
 }
--- src/test/java/self/ed/repository/UserRepositoryTest.java
+++ src/test/java/self/ed/repository/UserRepositoryTest.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 import self.ed.entity.Comment;
 import self.ed.entity.Post;
@@ -13,10 +16,14 @@ import self.ed.entity.User;
 import self.ed.testing.support.EntityFactory;
 import self.ed.testing.support.EntityHelper;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static self.ed.testing.support.RandomUtils.random;
 
@@ -157,4 +164,115 @@ public class UserRepositoryTest {
         assertThat(entityHelper.find(user)).isNull();
         assertThat(entityHelper.find(comment)).isNull();
     }
+
+    @Test
+    public void testFindAll_Page() {
+        IntStream.range(0, 3 * 5).forEach(i -> entityFactory.createUser());
+
+        Page<User> page = instance.findAll(PageRequest.of(1, 5));
+
+        List<User> expected = entityHelper.findAll(User.class);
+        assertThat(page.getTotalElements()).isEqualTo(expected.size());
+        assertThat(page.getTotalPages()).isEqualTo((int) Math.ceil(expected.size() / 5d));
+        assertThat(page.getContent().size()).isEqualTo(5);
+    }
+
+    @Test
+    public void testFindAll_Sort() {
+        IntStream.range(0, 3 * 5).forEach(i -> entityFactory.createUser());
+
+        Iterable<User> actual = instance.findAll(Sort.by("id"));
+
+        List<User> expected = entityHelper.findAll(User.class).stream()
+                .sorted(comparing(User::getId))
+                .collect(toList());
+        assertThat(actual).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    public void testFindAll_SortAndPage() {
+        IntStream.range(0, 3 * 5).forEach(i -> entityFactory.createUser());
+
+        Page<User> page = instance.findAll(PageRequest.of(1, 5, Sort.by("id")));
+
+        List<User> all = entityHelper.findAll(User.class);
+        List<User> expected = entityHelper.findAll(User.class).stream()
+                .sorted(comparing(User::getId))
+                .skip(5)
+                .limit(5)
+                .collect(toList());
+        assertThat(page.getTotalElements()).isEqualTo(all.size());
+        assertThat(page.getTotalPages()).isEqualTo((int) Math.ceil(all.size() / 5d));
+        assertThat(page.getContent()).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    public void testFindAllByName_Page() {
+        IntStream.range(0, 3 * 5).forEach(i -> entityFactory.createUser());
+        User user = entityFactory.createUser();
+
+        Page<User> page = instance.findAllByName(user.getName(), PageRequest.of(0, 5));
+
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getTotalPages()).isEqualTo(1);
+        assertThat(page.getContent()).containsExactly(user);
+    }
+
+    @Test
+    public void testFindAllByNameIn() {
+        IntStream.range(0, 3).forEach(i -> entityFactory.createUser());
+        List<User> searchUsers = IntStream.range(0, 3)
+                .mapToObj(i -> entityFactory.createUser())
+                .collect(toList());
+        List<String> searchNames = searchUsers.stream()
+                .map(User::getName)
+                .collect(toList());
+
+        List<User> found = instance.findAllByNameIn(searchNames);
+
+        assertThat(found).containsOnlyElementsOf(searchUsers);
+    }
+
+    @Test
+    public void testCountByName() {
+        IntStream.range(0, 3).forEach(i -> entityFactory.createUser());
+        User user = entityFactory.createUser();
+
+        long count = instance.countByName(user.getName());
+
+        assertThat(count).isEqualTo(1L);
+    }
+
+    @Test
+    public void testDeleteByName() {
+        User user = entityFactory.createUser();
+
+        long deleted = instance.deleteByName(user.getName());
+
+        assertThat(deleted).isEqualTo(1L);
+    }
+
+    @Test
+    public void testDeleteByName_NoMatches() {
+        long deleted = instance.deleteByName(random(String.class));
+
+        assertThat(deleted).isEqualTo(0);
+    }
+
+    @Test
+    public void testRemoveByName() {
+        User user = entityFactory.createUser();
+
+        List<User> removed = instance.removeByName(user.getName());
+
+        assertThat(removed).containsOnly(user);
+    }
+
+    @Test
+    public void testRemoveByName_NoMatches() {
+        List<User> removed = instance.removeByName(random(String.class));
+
+        assertThat(removed).isEmpty();
+    }
+
 }
