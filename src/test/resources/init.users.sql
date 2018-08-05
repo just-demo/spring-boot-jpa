8c4e289ecd404be65ba65bfd92d7b33f37617d24
Anatolii <anatolii.kryvda@gmail.com>
2018-08-05 15:05:28 +0300

    Other inmemory databases

--- src/test/resources/init.users.sql
+++ src/test/resources/init.users.sql
@@ -6,5 +6,16 @@ delete from user;
 
 alter sequence user_id_seq restart with 1;
 
+-- H2
 insert into user (id, name) values (user_id_seq.nextval, 'user1');
 insert into user (id, name) values (user_id_seq.nextval, 'user2');
+
+-- HSQLDB
+--insert into user (id, name) values (next value for user_id_seq, 'user1');
+--insert into user (id, name) values (next value for user_id_seq, 'user2');
+
+-- DERBY
+--drop sequence users_id_seq restrict;
+--create sequence users_id_seq start with 1;
+--insert into users (id, name) values (next value for users_id_seq, 'user1');
+--insert into users (id, name) values (next value for users_id_seq, 'user2');
