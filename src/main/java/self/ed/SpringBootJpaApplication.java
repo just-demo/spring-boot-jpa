991779f5a5435e2b26f2c3956cb91260ed76c78e
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2018-08-06 16:01:51 +0300

    Adding spring boot admin

--- src/main/java/self/ed/SpringBootJpaApplication.java
+++ src/main/java/self/ed/SpringBootJpaApplication.java
@@ -1,9 +1,11 @@
 package self.ed;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableAdminServer
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootJpaApplication {
