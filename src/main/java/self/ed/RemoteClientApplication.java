dc05116e07586490b2830b1d9df31f47ea0c9807
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2018-08-09 13:06:55 +0300

    Successful use of remote devtools

--- src/main/java/self/ed/RemoteClientApplication.java
+++ src/main/java/self/ed/RemoteClientApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.devtools.RemoteSpringApplication;
 
 public class RemoteClientApplication {
     public static void main(String[] args) {
-        RemoteSpringApplication.main(new String[]{"https://localhost:8080"});
+        // 1. run SpringBootJpaApplication as java -jar
+        // 2. run RemoteClientApplication from IDE
+        // 3. Build > Rebuild Project
+        // 4. The changes are pushed to SpringBootJpaApplication
+        RemoteSpringApplication.main(new String[]{"http://localhost:8080"});
     }
 }
